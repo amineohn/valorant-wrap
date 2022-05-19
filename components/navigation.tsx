866736fea376ac9b080d337e6d7e621f5dcc205b
amineohn <amineprojet7@gmail.com>
2022-05-19 15:22:10 +0200

    uw

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -205,7 +205,7 @@ const Navigation = () => {
           >
             <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#FD4556] shadow absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
             <span className="relative w-full text-left text-white uppercase transition-colors duration-300 ease-in-out group-hover:text-white">
-              Information release
+              Current Version
             </span>
           </button>
         </div>
